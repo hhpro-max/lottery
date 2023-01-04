083e2689f3aab457ac10095d5d7feeb9c3e1d1fd
hhpro <seyed.h.hashemi1@gmail.com>
2023-01-04 04:44:23 -1000

    a

--- lib/features/auth/screens/sing_in_screen.dart
+++ lib/features/auth/screens/sing_in_screen.dart
@@ -2,42 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:lottery/common/widgets/custom_common_elevatedbutton.dart';
 import 'package:lottery/common/widgets/custom_common_textbutton.dart';
 import 'package:lottery/common/widgets/custom_common_textformfield.dart';
-import 'package:lottery/helpers/applogger/app_logger.dart';
-
+import 'package:get/get.dart';
+import 'package:lottery/common/widgets/loading_opacity_screen.dart';
+import 'package:lottery/helpers/config.dart';
 class SigninScreen extends StatelessWidget {
-  const SigninScreen({super.key});
-
+  SigninScreen({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sign In"),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.all(12.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          const Text("Sign In To Your Account"),
-          const SizedBox(
-            height: 20,
-          ),
-          customCommonTextFormField(labelText: "username or Email Address"),
-          const SizedBox(
-            height: 20,
-          ),
-          customCommonTextFormField(labelText: "password"),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              customCommonTextButton(title: "or Sign Up",onPressed: (){}),
-              customCommonElevatedButton(title: "Sign In", onPressed: () {},),
-            ],
-          ),
-          
-        ]),
+      body: Stack(
+        children:[ Container(
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.all(12.0),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const Text("Sign In To Your Account"),
+            const SizedBox(
+              height: 20,
+            ),
+            customCommonTextFormField(labelText: "username or Email Address"),
+            const SizedBox(
+              height: 20,
+            ),
+            customCommonTextFormField(labelText: "password"),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                customCommonTextButton(title: "or Sign Up",onPressed: (){}),
+                customCommonElevatedButton(title: "Sign In", onPressed: () {
+                      
+                },),
+              ],
+            ),
+            
+          ]),
+        ),
+        Obx(()=>Get.find<Config>().waitingForLoginRes.value ==false?Container():const LoadingOpacityScreen())
+        ]
       ),
     );
   }
--- lib/features/auth/services/auth_services.dart
+++ lib/features/auth/services/auth_services.dart
@@ -3,19 +3,19 @@ import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:lottery/helpers/config.dart';
-class AuthServices{
-  static void signIn(
-    String userName,
-    String password
-  )async{
-    http.Response response = await http.post(Uri.parse("${Get.find<Config>().serverUrl}/signin"),
-    body: jsonEncode({
-      "userName":userName,
-      "password":password
-    }),
-    headers:{
-      'Content-Type': 'application/json; charset=UTF-8'
-  }
-    );
+
+class AuthServices {
+  static void signIn(String userName, String password) async {
+    Get.find<Config>().waitingForLoginRes.value = true;
+    try {
+      http.Response response = await http.post(
+          Uri.parse("${Get.find<Config>().serverUrl}/signin"),
+          body: jsonEncode({"userName": userName, "password": password}),
+          headers: {'Content-Type': 'application/json; charset=UTF-8'});
+          //todo 
+      Get.find<Config>().waitingForLoginRes.value = false;    
+    } catch (e) {
+      Get.find<Config>().waitingForLoginRes.value = false;
+    }
   }
 }
