931d82fd081edf65b5afe96fb5fe4b27f8d49c55
hhpro <seyed.h.hashemi1@gmail.com>
2023-01-06 04:06:04 -1000

    b

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,15 +1,26 @@
 import 'package:lottery/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider {
+  String _token = "";
   Map<String,String> headers = {'Content-Type': 'application/json; charset=UTF-8'};
-
+  SharedPreferences? _preferences;
   User _user = User( id: "", userName: "", email: "", password: "", cart: []);
   User get user => _user;
   void setUser(String user) {
     _user = User.fromJson(user);
   }
-
+  Future<SharedPreferences> getPrefereces()async{
+    _preferences ??= await SharedPreferences.getInstance();
+    return _preferences!;
+  }
   void reSetUser() {
     _user = User(id: "",userName: "", email: "", password: "", cart: []);
+  }
+  String get token => _token;
+  setToken(String token)async{
+      var prefs = await getPrefereces();
+      await prefs.setString("x-auth-token", token);
+    _token = token;
   }
 }
